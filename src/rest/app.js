5d441d0a18e9016761863cc702c1c750df2e77f2
helenjym <helenjym@gmail.com>
2026-01-07 23:36:12 -0800

    add backend validation and styling

--- src/rest/app.js
+++ src/rest/app.js
@@ -5,12 +5,59 @@ const app = express();
 const port = 3000;
 const { InvoicePayment, InvoicePaymentRequisition } = require( '../controller/InvoiceProcessor.ts');
 const fs = require('fs'); 
-// Changing filename to be the same for every uplffoad--any existing file gets replaced
+const {body, validationResult, check} = require('express-validator');
+
+
+const createEmailChain = () => body('email').isEmail();
+// file name is the same every upload--any existing file gets replaced
 const storage=multer.diskStorage({
     destination:(req,file,cb)=>cb(null,'../uploads'),
-    filename:(req,file,cb)=>cb(null,'Invoice.pdf')
+    fileFilter: (req, file, cb) => {
+        if (file.fieldname !== "invoice" && file.fieldname !== "voidCheque") {
+            cb(null,false);
+        } else {
+            cb(null, true);
+        }
+    },
+    filename:(req,file,cb)=> {
+        if (file.fieldname === "invoice") {
+            cb(null,'Invoice.pdf')
+        }
+        if (file.fieldname === "voidCheque") {
+            cb(null, 'VoidCheque.pdf')
+        }
+    }
 });
-// validate file
+
+// code adapted from https://medium.com/@sridhar_be/file-validations-using-magic-numbers-in-nodejs-express-server-d8fbb31a97e7
+function validateUploadedFile(file) {
+    if (file.size > 5*1024*1024) {
+        console.log("File too large")
+        return false;
+    }    
+    try {
+        const fd = fs.openSync('../uploads/Invoice.pdf');
+        try {
+            const buffer = Buffer.alloc(5);
+            fs.readSync(fd, buffer, 0, 5, 0);
+            const hexSignature = buffer.toString('hex').toUpperCase();
+            const signature = '255044462D';
+            console.log(hexSignature)
+            fs.closeSync(fd)
+            const isValid = (signature === hexSignature)
+            console.log(isValid);
+            return isValid; 
+        } catch(err) {
+            console.log(err);
+            fs.closeSync(fd);
+            return false;
+        }
+    } catch(e) {
+        console.log(e);
+        return false;
+    }
+}
+
 
 const upload = multer({storage});
 
@@ -20,17 +67,53 @@ app.get('/invoice', (req, res) => {
     res.send("Hello");
 });
 
-app.post('/upload', upload.single('invoice'), async (req, res) => {
-    console.log(req.body, req.file)
-    const invoiceNum = Number(req.body.invoiceNum);
-    const gst = Number(req.body.gst);
-    const total = Number(req.body.total);
-    const accCode = Number(req.body.accCode);
+app.post('/upload', upload.fields([
+    {name: 'invoice', maxCount: 1},
+    {name: 'voidCheque', maxCount: 1}
+]), [
+    check("supplierName").trim().notEmpty().matches(/^[A-Za-z0-9',&-\s]+$/)
+    .isLength({max: 500}),
+    createEmailChain(),
+    check("date").isDate([strictMode="true"]),
+    check("invoiceNum").trim().notEmpty().matches(/^[A-Za-z0-9-\s]+$/).isLength({max: 500}),
+    check("purpose").trim().notEmpty().matches(/^[A-Za-z0-9',-./!\s]+$/).isLength({max: 500}),
+    check("accCode").trim().notEmpty().matches(/^[0-9]+$/).isLength(5),
+    check("gst").trim().notEmpty().matches(/^[0-9.]+$/).isLength({max: 500}),
+    check("total").trim().notEmpty().matches(/^[0-9.]+$/).isLength({max: 500}),
+    check("treasurerName").trim().notEmpty().matches(/^[A-Za-z\s]+$/)
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array);
+        return res.status(400).json({errors: errors.array()});
+    }
+    if (req.files["invoice"] === undefined) {
+        return res.status(400).send("No invoice uploaded");
+    }
+    if (!validateUploadedFile(req.files["invoice"][0])) {
+        return res.status(400).send("Error in invoice file format");
+    }
+    const voidChequeAttached = (req.files["voidCheque"] !== undefined);
+    console.log(voidChequeAttached);
+    if (voidChequeAttached) {
+        const validVoidCheque = validateUploadedFile(req.files["voidCheque"][0]);
+        if (!validVoidCheque) {
+            return res.status(400).send("Error in void cheque file format");
+        }
+    }
     try {
-        const invoicePayment = InvoicePayment.createGeneralInvoice(req.body.supplierName, invoiceNum, req.body.date, gst, total, req.body.email, accCode, req.body.purpose, req.body.treasurerName);
-        const IPR = await InvoicePaymentRequisition.create(invoicePayment)
-        const bytes = await IPR.attachInvoice();
-        fs.writeFileSync("IPR_Merged.pdf", bytes, 'utf-8');
+        const gst = Number(req.body.gst);
+        const total = Number(req.body.total);
+        const accCode = Number(req.body.accCode);    
+        const invoicePayment = InvoicePayment.createGeneralInvoice(req.body.supplierName, req.body.invoiceNum, req.body.date, gst, total, req.body.email, accCode, req.body.purpose, req.body.treasurerName);
+        const IPR = await InvoicePaymentRequisition.create(invoicePayment);
+        // sending a file path is relative to the intiial call? oh wait that makes sense
+        let bytes = await IPR.attachSupportingDoc("../uploads/Invoice.pdf");
+        if (voidChequeAttached) {
+            bytes = await IPR.attachSupportingDoc("../uploads/VoidCheque.pdf");
+        }
+        fs.writeFileSync("IPR_Merged.pdf"
+            , bytes, 'utf-8');
         const jsonData = JSON.stringify(invoicePayment);
         fs.writeFileSync("InvoicePayment.json", jsonData, 'utf-8');
         res.status(200).send("Upload succesful");
@@ -40,7 +123,10 @@ app.post('/upload', upload.single('invoice'), async (req, res) => {
     }
 });
 
-app.post('/upload/Horizon', upload.single('invoice'), async (req, res) => {
+app.post('/upload/Horizon', upload.single('invoice'), [check("purpose").trim().notEmpty().matches(/^[A-Za-z0-9',-./!\s]+[A-Za-z0-9',-./!\s]$/).isLength({max: 500}),
+    check("accCode").trim().notEmpty().matches(/^[0-9]+$/).isLength(5),
+    check("treasurerName").trim().notEmpty().matches(/^[A-Za-z\s]+$/)
+], async (req, res) => {
     console.log(req.body, req.file)
     const accCode = Number(req.body.accCode);
     try {
@@ -63,7 +149,8 @@ app.post('/upload/Horizon', upload.single('invoice'), async (req, res) => {
     }
 });
 
-app.get('/download/:iv', (req, res) => {
+app.get('/download/:iv', check("iv").trim().notEmpty().matches(/^[A-Za-z0-9-\s]+[A-Za-z0-9-]$/).isLength({max: 500})
+, (req, res) => {
     try {
         const invoiceFile = fs.readFileSync("./InvoicePayment.json", "utf-8");
         const invoicePayment = JSON.parse(invoiceFile);
@@ -76,6 +163,7 @@ app.get('/download/:iv', (req, res) => {
             res.write(merged, 'utf8', () => {console.log("Sent");});
             res.end();
         }
+
     } catch(e) {
         console.log(e);
         res.status(500).send("Error occurred while downloading");
@@ -84,7 +172,7 @@ app.get('/download/:iv', (req, res) => {
 
 app.get('/suppliers', (req, res) => {
     try {
-        const suppliersFile = fs.readFileSync('../suppliers.json', "utf-8");
+        const suppliersFile = fs.readFileSync('../data/suppliers.json', "utf-8");
         // returns a utf-8 character encoding of the json file
         const suppliers = JSON.parse(suppliersFile);
         res.json(suppliers);

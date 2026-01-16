cc479ab0fb0ceff5fae3d1da65236959f3e2caac
helenjym <helenjym@gmail.com>
2026-01-15 21:56:53 -0800

    use env variable for server host and commit changes lol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   async function fetchData() {
     try {
-      const response = await fetch(import.meta.env.VITE_BACKEND_HOST + "/suppliers");
+      const response = await fetch(import.meta.env.BACKEND_URL + "/suppliers");
       const suppliersObj = await response.json();
       setSuppliers(suppliersObj);
       setConnectionError(false);
@@ -218,7 +218,7 @@ function GeneralSupplierForm({supplier}) {
         form.append("voidCheque", voidCheque);
       }
       try {
-        const uploadResponse = await fetch('http://localhost:3010/upload', {
+        const uploadResponse = await fetch(import.meta.env.BACKEND_URL + "/upload", {
           method: "POST",
           body: form,
         });
@@ -227,7 +227,7 @@ function GeneralSupplierForm({supplier}) {
           console.log(uploadResponseText);
           throw new Error("File upload error");
         }
-        const downloadResponse = await fetch('http://localhost:3010/download/' + invoiceNum);
+        const downloadResponse = await fetch(import.meta.env.BACKEND_URL + '/download/' + invoiceNum);
         if (!downloadResponse.ok) {
           const downloadResponseText = await downloadResponse.text();
           console.log(downloadResponseText);  
@@ -356,7 +356,7 @@ function ProcessableSupplierForm({supplier}) {
     form.append("treasurerName", name);
     form.append("invoice", file);
     try {
-      const uploadResponse = await fetch('http://localhost:3010/upload/' + supplierName, {
+      const uploadResponse = await fetch(import.meta.env.BACKEND_URL + "/upload/" + supplierName, {
         method: "POST",
         body: form,
       });
@@ -366,7 +366,7 @@ function ProcessableSupplierForm({supplier}) {
         throw new Error(`Response status: ${uploadResponse.status}`);
       } else {
         setInvoiceNum(uploadResponseText);
-        const downloadResponse = await fetch('http://localhost:3010/download/' + uploadResponseText);
+        const downloadResponse = await fetch(import.meta.env.BACKEND_URL + '/download/' + uploadResponseText);
         if (!downloadResponse.ok) {
           console.log(downloadResponse.statusText);
           throw new Error(`Response status: ${downloadResponse.status}`);
--- frontend/src/components/ProcessableSupplierForm.jsx
+++ frontend/src/components/ProcessableSupplierForm.jsx
@@ -20,7 +20,7 @@ export default function ProcessableSupplierForm({supplier}) {
     form.append("treasurerName", name);
     form.append("invoice", file);
     try {
-      const uploadResponse = await fetch('http://localhost:3000/upload/' + supplierName, {
+      const uploadResponse = await fetch(import.meta.env.BACKEND_URL + "/upload/" + supplierName, {
         method: "POST",
         body: form,
       });
@@ -30,7 +30,7 @@ export default function ProcessableSupplierForm({supplier}) {
         throw new Error(`Response status: ${uploadResponse.status}`);
       } else {
         setInvoiceNum(uploadResponseText);
-        const downloadResponse = await fetch('http://localhost:3000/download/' + uploadResponseText);
+        const downloadResponse = await fetch(import.meta.env.BACKEND_URL + "/download/" + uploadResponseText);
         if (!downloadResponse.ok) {
           console.log(downloadResponse.statusText);
           throw new Error(`Response status: ${downloadResponse.status}`);
